e70105a554ab3df14dd7fcaf8e09d5a73a4b457a
Misha <Misha140699@mail.ru>
2023-09-08 11:40:15 +0300

    add list of articles classes

--- src/pages/listOfArticles/index.js
+++ src/pages/listOfArticles/index.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-//import s from "./listOfArticles.module.scss";
+import s from "./listOfArticles.module.scss";
 const ListOfArticles = () => {
   return (
-    <div class="wrapper">
-      <div class="content">
-        <div class="columns">
-          <div class="container">
-            <h1 class="vertical_title">Список статей</h1>
-            <div class="vertical_body">
-              <div class="vertical_row">
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <div className={s.columns}>
+          <div className={s.container}>
+            <h1 className={s.vertical_title}>Список статей</h1>
+            <div className={s.vertical_body}>
+              <div className={s.vertical_row}>
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
@@ -28,17 +28,17 @@ const ListOfArticles = () => {
                   </div>
                 </div>
 
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -47,17 +47,17 @@ const ListOfArticles = () => {
                   </div>
                 </div>
 
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -66,18 +66,18 @@ const ListOfArticles = () => {
                   </div>
                 </div>
               </div>
-              <div class="vertical_row_second">
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+              <div className={s.vertical_row_second}>
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -85,17 +85,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -103,17 +103,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -123,18 +123,18 @@ const ListOfArticles = () => {
                 </div>
               </div>
 
-              <div class="vertical_row_tree">
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+              <div className={s.vertical_row_tree}>
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -142,17 +142,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -160,17 +160,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -179,18 +179,18 @@ const ListOfArticles = () => {
                   </div>
                 </div>
               </div>
-              <div class="vertical_row_four">
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+              <div className={s.vertical_row_four}>
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -198,17 +198,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -216,17 +216,17 @@ const ListOfArticles = () => {
                     </div>
                   </div>
                 </div>
-                <div class="vertical_column">
-                  <div class="vertical_item">
-                    <div class="vertical_subtitle"></div>
-                    <div class="vertical_label">
+                <div className={s.vertical_column}>
+                  <div className={s.vertical_item}>
+                    <div className={s.vertical_subtitle}></div>
+                    <div className={s.vertical_label}>
                       Очень длинный заголовок, хочется читать
                     </div>
-                    <div class="vertical_date">
+                    <div className={s.vertical_date}>
                       01.12.2021{" "}
                       <Link
                         to="http://127.0.0.1:8080/pages/article"
-                        class="vertical_date_link"
+                        className={s.vertical_date_link}
                       >
                         ・Читать
                       </Link>{" "}
@@ -239,8 +239,8 @@ const ListOfArticles = () => {
           </div>
         </div>
 
-        <div class="read_more">
-          <Link to="#" class="read_more_text"></Link>
+        <div className={s.read_more}>
+          <Link to="#" className={s.read_more_text}></Link>
           <p>
             Смотреть все{" "}
             <img
@@ -248,49 +248,48 @@ const ListOfArticles = () => {
               width=" 24px"
               height=" 24px"
               alt=""
-              class="icon"
+              className={s.icon}
             ></img>
           </p>
         </div>
       </div>
 
-      <div class="what">
+      <div className={s.what}>
         <img
           src="img/in2sql.svg"
           width="244px"
           height="204px"
           alt="in2sql"
-          class="insql_image"
+          className={s.insql_image}
         ></img>
-        <div class="what_row">
-          <a href="#" class="link_title">
+        <div className={s.what_row}>
+          <Link to="#" className={s.link_title}>
             <h1>Попробуй in2sql прямо сейчас</h1>
-          </a>
+          </Link>
 
-          <p class="about_sql">
+          <p className={s.about_sql}>
             Клиент для интеграции excel c внешними источниками данных.
           </p>
 
-          <div class="action">
-            <div class="one_month">
-              <p class="one_month_text_days">30 дней</p>
-              <p class="one_month_text">создавай свои отчеты</p>
+          <div className={s.action}>
+            <div className={s.one_month}>
+              <p className={s.one_month_text_days}>30 дней</p>
+              <p className={s.one_month_text}>создавай свои отчеты</p>
             </div>
-            <div class="price">
-              <p class="price_number">0 Р</p>
-              <p class="action_time"> акция до декабря 2022</p>
+            <div className={s.price}>
+              <p className={s.price_number}>0 Р</p>
+              <p className={s.action_time}> акция до декабря 2022</p>
             </div>
-            <div class="empty_div">
-              <p class="empty_div_text"></p>
+            <div className={s.empty_div}>
+              <p className={s.empty_div_text}></p>
             </div>
           </div>
 
           <button
             onclick="window.location.href ='http://127.0.0.1:8080/pages/questionnaire.html';"
-            class="button_free"
+            className={s.button_free}
           >
-            {" "}
-            <p class="button_free_text">Попробовать бесплатно</p>
+            <p className={s.button_free_text}>Попробовать бесплатно</p>
           </button>
         </div>
       </div>
